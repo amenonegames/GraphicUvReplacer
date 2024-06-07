806a75e0b1d7ffc1a2f3e49610e971f012ed6731
amenonegames <amenone.games@gmail.com>
2024-06-08 02:25:07 +0900

    UnityProject用意　基本クラス生成

---
+++ Assets/Packages/GraphicUvReplacer/Scripts/MeshEffect/UiUV0uvzwReplacer.cs
@@ -0,0 +1,61 @@
+﻿using System.Buffers;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+using UnityEngine.UI;
+
+namespace View.DialogueView.MeshEffect
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(Graphic))]
+    public class UiUV0uvzwReplacer : BaseMeshEffect
+    {
+
+        private Vector4 uvzw;
+
+        public Vector4 UVXW
+        {
+            get { return uvzw; }
+            set
+            {
+                uvzw = value;
+                if (graphic != null)
+                    graphic.SetVerticesDirty();
+            }
+        }
+        
+        public override void ModifyMesh(VertexHelper vh)
+        {
+            if (!IsActive())
+                return;
+
+            var output = ListPool<UIVertex>.Get();
+            vh.GetUIVertexStream(output);
+            ApplyVerts(output);
+            vh.Clear();
+            vh.AddUIVertexTriangleStream(output);
+            ListPool<UIVertex>.Release(output);
+        }
+
+        private void ApplyVerts(List<UIVertex> verts)
+        {
+            UIVertex vt;
+            Vector4[] vec4 = ArrayPool<Vector4>.Shared.Rent(verts.Count);
+
+            for (int i = 0; i < verts.Count; ++i)
+            {
+                vt = verts[i];
+                vec4[i].x = UVXW.x;
+                vec4[i].y = UVXW.y;
+                vec4[i].z = UVXW.z;
+                vec4[i].w = UVXW.w;
+                
+                vt.uv0 = vec4[i];
+                verts[i] = vt;
+            }
+
+            ArrayPool<Vector4>.Shared.Return(vec4);
+
+        }
+    }
+}
---
+++ Assets/Packages/GraphicUvReplacer/Scripts/MeshEffect/UiUV0zwReplacer.cs
@@ -0,0 +1,63 @@
+﻿using System.Buffers;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+using UnityEngine.UI;
+
+namespace View.DialogueView.MeshEffect
+{
+    [DisallowMultipleComponent]
+    [RequireComponent(typeof(Graphic))]
+    public class UiUV0zwReplacer : BaseMeshEffect
+    {
+
+        private Vector2 zw;
+
+        public Vector2 ZW
+        {
+            get { return zw; }
+            set
+            {
+                zw = value;
+                if (graphic != null)
+                    graphic.SetVerticesDirty();
+            }
+        }
+        
+        public override void ModifyMesh(VertexHelper vh)
+        {
+            if (!IsActive())
+                return;
+
+            var output = ListPool<UIVertex>.Get();
+            vh.GetUIVertexStream(output);
+            ApplyVerts(output);
+            vh.Clear();
+            vh.AddUIVertexTriangleStream(output);
+            ListPool<UIVertex>.Release(output);
+        }
+
+        private void ApplyVerts(List<UIVertex> verts)
+        {
+            UIVertex vt;
+            Vector4[] vec4 = ArrayPool<Vector4>.Shared.Rent(verts.Count);
+
+            for (int i = 0; i < verts.Count; ++i)
+            {
+                vt = verts[i];
+                vec4[i].x = vt.uv0.x;
+                vec4[i].y = vt.uv0.y;
+                vec4[i].z = ZW.x;
+                vec4[i].w = ZW.y;
+                
+                vt.uv0 = vec4[i];
+                verts[i] = vt;
+            }
+
+            ArrayPool<Vector4>.Shared.Return(vec4);
+
+        }
+        
+        
+    }
+}
