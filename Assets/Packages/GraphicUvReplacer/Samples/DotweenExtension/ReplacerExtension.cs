393751a7e003dc998abdc7eb2b1e447837c81803
amenonegames <amenone.games@gmail.com>
2024-06-08 03:29:58 +0900

    Dotween導入とSample借り入れ

---
+++ Assets/Packages/GraphicUvReplacer/Samples/DotweenExtension/ReplacerExtension.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using Packages.GraphicUvReplacer.Scripts.Abstract;
+using UnityEngine;
+
+namespace Packages.GraphicUvReplacer.Samples.DotweenExtension
+{
+    public static class ReplacerExtension
+    {
+        public static async UniTask EaseZAsync(this ZwReplacer replacer, Vector2 endValue, float duration, CancellationToken token)
+        {
+            await DOTween.To(() => replacer.ZW, (x) => replacer.ZW = x, endValue, duration);
+        } 
+    }
+}
